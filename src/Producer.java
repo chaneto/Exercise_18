0f75be9b99fede43d7e8623f5bd586479f288b3c
Stoyan Stoyanov <chaneto800ss@gmail.com>
2022-05-18 19:35:21 +0300

    update

--- src/Producer.java
+++ src/Producer.java
@@ -1,19 +1,19 @@
-public class Producer<T> implements Runnable{
+public class Producer<T> implements Runnable {
 
-    private final ArrayBlockingQueue sharedQueue;
+  private final ArrayBlockingQueue sharedQueue;
 
-    public Producer(ArrayBlockingQueue sharedQueue) {
-        this.sharedQueue = sharedQueue;
+  public Producer(ArrayBlockingQueue sharedQueue) {
+    this.sharedQueue = sharedQueue;
+  }
+
+  @Override
+  public void run() {
+    try {
+      sharedQueue.put(Thread.currentThread().getName());
+      System.out.println("Produced: " + Thread.currentThread().getName());
+    } catch (InterruptedException ex) {
+      System.out.println(ex.getMessage());
     }
-
-    @Override
-    public void run() {
-            try {
-                sharedQueue.put(Thread.currentThread().getName());
-                System.out.println("Produced: " + Thread.currentThread().getName());
-            } catch (InterruptedException ex) {
-                System.out.println(ex.getMessage());
-            }
-    }
+  }
 
 }
